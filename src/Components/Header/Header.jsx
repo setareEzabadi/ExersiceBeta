ce9063982fcea6e36d31788e527a9cf48843ae89
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-08-29 09:32:38 +0330

    Update Header.jsx

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+// const Header = () => {
+//     return (
+//         <div className="Header">
+//             <span>salam header</span>
+//         </div>
+//     );
+// };
+
+// export default Header;
+
+const Header = () => {
+    return (
+        <div className="bg-blue-500 text-white p-4 rounded-lg">
+            سلام! این یک مثال ساده از استفاده از Tailwind CSS است.
+        </div>
+    );
+};
+
+export default Header;
