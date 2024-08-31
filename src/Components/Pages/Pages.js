2b4c362547d25d77aa9f3a2b6090ce35bb27c40c
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-08-31 08:46:27 +0330

    Update Pages.js

--- src/Components/Pages/Pages.js
+++ src/Components/Pages/Pages.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import MainPage from "./MainPage/MainPage";
+import Wallet from "../Wallet.jsx/Wallet";
 
 const Pages = () => {
   return (
     <Routes>
       <Route exact path="/" element={<MainPage />}>
-        {/* <Route path="/teacher" element={<UserCounter />} /> */}
+        <Route path="/Wallet" element={<Wallet />} />
       </Route>
     </Routes>
   );
