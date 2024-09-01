c36a7849e11a603c21a0c79321b37c6ae4113ef8
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-09-01 03:40:53 +0330

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,13 +8,13 @@ import menu from '../../Assets/img/menue.png';
 
 const Header = () => {
     return (
-        <div className="flex items-center justify-between py-2">
+        <div className="flex items-center justify-between">
 
             {/* Right Side: Balance and Profile */}
             <div className="flex items-center gap-6">
 
                 {/* Profile Section - Hidden on md and smaller */}
-                <div className="hidden md:flex items-center bg-[#E8E8E8] px-4 rounded-lg h-[55px] gap-[26px]">
+                <div className="hidden md:flex items-center bg-[#E8E8E8] px-4 rounded-lg h-[45px] gap-[26px]">
                     <div className="flex items-center gap-[15px]">
                         <img src={profile} alt="Profile" className="w-8 h-8 rounded-full mr-2" />
                         <span className="text-[#727272] text-[14px] font-bold">ابوالفضل عزیز خوش آمدی</span>
@@ -28,7 +28,7 @@ const Header = () => {
                 </div>
 
                 {/* Balance Section */}
-                <div className="flex items-center bg-[#E8E8E8] px-4 rounded-lg h-[55px]">
+                <div className="flex items-center bg-[#E8E8E8] px-4 rounded-lg h-[45px]">
                     <span className="text-[#727272] text-[14px] font-bold ml-2">21,265,000 تومان</span>
                     <img src={wallet} alt="Wallet" className="w-6 h-6" />
                 </div>
@@ -39,13 +39,13 @@ const Header = () => {
             <div className="flex items-center gap-4">
 
                 {/* Return to Website Button */}
-                <button className="flex items-center bg-[#FF2B2B] text-white px-4 rounded-lg h-[55px]">
+                <button className="flex items-center bg-[#FF2B2B] text-white px-4 rounded-lg h-[45px]">
                     <span className="ml-2 text-[14px] font-bold md:block hidden">برگشت به وبسایت</span>
                     <img src={logOut} alt="Return Arrow" className="w-5 h-5" />
                 </button>
 
                 {/* Notification Icon with Badge */}
-                <div className="relative flex items-center bg-[#E8E8E8] p-2 rounded-md h-[55px] w-[55px]">
+                <div className="relative flex items-center bg-[#E8E8E8] p-2 rounded-md h-[45px] w-[45px]">
                     <img src={message} alt="Notification" className="w-6 h-6 mx-auto" />
                     <span className="absolute -top-1 -right-1 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
                         2
