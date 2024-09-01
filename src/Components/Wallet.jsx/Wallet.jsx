eb0db60cf153e6c241b15b8a240b7e4ff875d32c
setareEzabadi <127828381+setareEzabadi@users.noreply.github.com>
2024-09-01 03:41:05 +0330

    Update Wallet.jsx

--- src/Components/Wallet.jsx/Wallet.jsx
+++ src/Components/Wallet.jsx/Wallet.jsx
@@ -26,13 +26,13 @@ const Wallet = () => {
                 <img
                     src={cardImage}
                     alt="Card"
-                    className="rounded-xl shadow-lg xl:w-[527px] xl:h-[279px] w-[300px] h-[160px]"
+                    className="rounded-xl shadow-lg xl:w-[527px] xl:h-[279px] w-[300px] h-[145px]"
                 />
 
                 {/* Button Section */}
                 <div className="space-y-5 w-full">
                     {/* Selected Button */}
-                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[78px] w-full h-[60px] bg-[#262630] text-white text-[16px] font-bold rounded-2xl px-4">
+                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[63px] w-full h-[45px] bg-[#262630] text-white text-[16px] font-bold rounded-2xl px-4">
                         <span className="flex items-center gap-2">
                             <img src={check} alt="Check" className="w-[30px] h-[30px]" />
                             انتخاب شماره کارت حقیقی
@@ -40,14 +40,14 @@ const Wallet = () => {
                     </button>
 
                     {/* Unselected Buttons */}
-                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[78px] w-full h-[60px] bg-[#F1F1F1] text-[#C4C4C4] text-[16px] font-bold rounded-2xl px-4">
+                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[63px] w-full h-[45px] bg-[#F1F1F1] text-[#C4C4C4] text-[16px] font-bold rounded-2xl px-4">
                         <span className="flex items-center gap-2">
                             <div className="w-[30px] h-[30px] bg-[#D1D1D1] rounded-xl"></div>
                             انتخاب شماره کارت حقوقی
                         </span>
                     </button>
 
-                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[78px] w-full h-[60px] bg-[#F1F1F1] text-[#C4C4C4] text-[16px] font-bold rounded-2xl px-4">
+                    <button className="flex items-center justify-between xl:w-[454px] xl:h-[63px] w-full h-[45px] bg-[#F1F1F1] text-[#C4C4C4] text-[16px] font-bold rounded-2xl px-4">
                         <span className="flex items-center gap-2">
                             <div className="w-[30px] h-[30px] bg-[#D1D1D1] rounded-xl"></div>
                             انتخاب شماره کارت دلخواه
@@ -63,19 +63,19 @@ const Wallet = () => {
                     <input
                         type="text"
                         placeholder="* شماره پیگیری خود را وارد نمایید"
-                        className="w-full md:w-[313px] h-[60px] bg-[#F1F1F1] text-[#C4C4C4] text-[14px] font-bold rounded-xl px-4 focus:outline-none focus:border-[1.5px] focus:border-[#FF2A2A]"
+                        className="w-full md:w-[313px] h-[45px] bg-[#F1F1F1] text-[#C4C4C4] text-[14px] font-bold rounded-xl px-4 focus:outline-none focus:border-[1.5px] focus:border-[#FF2A2A]"
                     />
 
                     {/* Amount Input */}
                     <input
                         type="text"
                         placeholder="* مبلغ مد نظر خود را وارد نمایید"
-                        className="w-full md:w-[313px] h-[60px] bg-[#F1F1F1] text-[#C4C4C4] text-[14px] font-bold rounded-xl px-4 focus:outline-none focus:border-[1.5px] focus:border-[#FF2A2A]"
+                        className="w-full md:w-[313px] h-[45px] bg-[#F1F1F1] text-[#C4C4C4] text-[14px] font-bold rounded-xl px-4 focus:outline-none focus:border-[1.5px] focus:border-[#FF2A2A]"
                     />
 
                     {/* Date Input */}
-                    <div className="relative flex items-center w-full md:w-[313px] h-[60px] bg-[#F1F1F1] rounded-xl">
-                        <div className="absolute left-0 top-0 flex items-center justify-center w-[60px] h-[60px] bg-[#262630] rounded-xl">
+                    <div className="relative flex items-center w-full md:w-[313px] h-[45px] bg-[#F1F1F1] rounded-xl">
+                        <div className="absolute left-0 top-0 flex items-center justify-center w-[45px] h-[45px] bg-[#262630] rounded-xl">
                             <img src={date} alt="Date Icon" className="w-[30px] h-[30px]" />
                         </div>
                         <input
@@ -89,8 +89,8 @@ const Wallet = () => {
                 {/* Bottom Input Section */}
                 <div className="flex flex-col md:flex-row gap-3 items-center w-full">
                     {/* Upload Comment Input */}
-                    <div className="relative flex items-center w-full md:w-[313px] h-[60px] bg-[#F1F1F1] rounded-xl">
-                        <div className="absolute left-0 top-0 flex items-center justify-center w-[60px] h-[60px] bg-[#262630] rounded-xl">
+                    <div className="relative flex items-center w-full md:w-[313px] h-[45px] bg-[#F1F1F1] rounded-xl">
+                        <div className="absolute left-0 top-0 flex items-center justify-center w-[45px] h-[45px] bg-[#262630] rounded-xl">
                             <img src={copy} alt="Upload Icon" className="w-[30px] h-[30px]" />
                         </div>
                         <input
@@ -101,7 +101,7 @@ const Wallet = () => {
                     </div>
 
                     {/* Card Last 4 Digits Input - for md and above */}
-                    <div className="hidden md:flex relative flex items-center w-full md:w-[683px] h-[60px] bg-[#F1F1F1] rounded-xl px-4">
+                    <div className="hidden md:flex relative flex items-center w-full md:w-[683px] h-[45px] bg-[#F1F1F1] rounded-xl px-4">
                         <div className="flex-1 flex flex-row-reverse items-baseline gap-[24px] text-[#D9D9D9]">
                             <input
                                 type="text"
@@ -122,7 +122,7 @@ const Wallet = () => {
                                 </div>
                             ))}
                         </div>
-                        <button className="absolute right-0 flex items-center justify-center w-[265px] h-[60px] bg-[#FF2A2A] text-white text-[14px] font-bold rounded-xl">
+                        <button className="absolute right-0 flex items-center justify-center w-[265px] h-[45px] bg-[#FF2A2A] text-white text-[14px] font-bold rounded-xl">
                             4 رقم آخر شماره کارت وارد نمایید
                         </button>
                     </div>
@@ -131,7 +131,7 @@ const Wallet = () => {
                     <div className="flex md:hidden flex-col items-end gap-1 w-full">
                         {/* متن سمت راست بالای اینپوت */}
                         <p className="text-[#FF2A2A] self-start">4 رقم آخر شماره کارت وارد نمایید.</p>
-                        <div className="flex items-center w-full h-[60px] bg-[#F1F1F1] rounded-xl px-4">
+                        <div className="flex items-center w-full h-[45px] bg-[#F1F1F1] rounded-xl px-4">
                             <div className="flex-1 flex flex-row-reverse items-baseline gap-[24px] text-[#D9D9D9]">
                                 <input
                                     type="text"
@@ -158,8 +158,8 @@ const Wallet = () => {
 
                 <div className="flex flex-col items-center gap-4 w-full">
                     {/* IBAN Input - for md and above */}
-                    <div className="hidden md:flex flex flex-col md:flex-row items-center justify-between w-full md:w-[1010px] h-[60px] bg-[#F1F1F1] rounded-xl p-2">
-                        <div className="w-full md:w-[313px] h-[60px] bg-[#565662] flex items-center justify-center rounded-xl text-white text-[14px]">
+                    <div className="hidden md:flex flex flex-col md:flex-row items-center justify-between w-full md:w-[1010px] h-[45px] bg-[#F1F1F1] rounded-xl p-2">
+                        <div className="w-full md:w-[313px] h-[45px] bg-[#565662] flex items-center justify-center rounded-xl text-white text-[14px]">
                             24 رقم شماره شبا کارت بانکی خود را وارد نمایید
                         </div>
                         <div className="flex flex-col md:flex-row flex-1 gap-[30px] justify-center items-baseline">
@@ -181,7 +181,7 @@ const Wallet = () => {
                                 </div>
                             ))}
                         </div>
-                        <div className="w-[65px] h-[60px] bg-[#262630] flex items-center justify-center rounded-xl text-white text-[14px]">
+                        <div className="w-[65px] h-[45px] bg-[#262630] flex items-center justify-center rounded-xl text-white text-[14px]">
                             IR
                         </div>
                     </div>
@@ -190,8 +190,8 @@ const Wallet = () => {
                     <div className="flex md:hidden flex-col items-end gap-1 w-full">
                         {/* متن سمت راست بالای اینپوت */}
                         <p className="text-[#565662] self-start">24 رقم شماره شبا وارد نمایید</p>
-                        <div className="flex items-center w-full h-[60px] bg-[#F1F1F1] rounded-xl px-4">
-                            <div className="flex items-center justify-center w-[60px] h-[60px] bg-[#262630] rounded-xl text-white text-[14px] order-last ml-[-4%]">
+                        <div className="flex items-center w-full h-[45px] bg-[#F1F1F1] rounded-xl px-4">
+                            <div className="flex items-center justify-center w-[45px] h-[45px] bg-[#262630] rounded-xl text-white text-[14px] order-last ml-[-4%]">
                                 IR
                             </div>
                             <div className="flex items-center flex-col flex-1 gap-1">
@@ -226,7 +226,7 @@ const Wallet = () => {
                 </div>
                 {/* Updated Position of Submit Button */}
                 <div className="mt-2 w-full flex justify-start max-w-[1024px] mx-auto">
-                    <button className="w-full md:w-[130px] md:h-[60px] h-[40px] w-[120px] bg-[#1FBC42] text-white text-[14px] font-bold rounded-xl">
+                    <button className="w-full md:w-[130px] md:h-[45px] h-[45px] w-[120px] bg-[#1FBC42] text-white text-[14px] font-bold rounded-xl">
                         ارسال اطلاعات
                     </button>
                 </div>
